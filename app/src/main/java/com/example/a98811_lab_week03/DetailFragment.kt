55186a6ac6fc539b0a45a69efd56869efad4a5dd
kkaveman <kevinanatta1@gmail.com>
2025-09-18 06:15:24 +0700

    Commit No.4 - Use jetpack navigation

--- app/src/main/java/com/example/a98811_lab_week03/DetailFragment.kt
+++ app/src/main/java/com/example/a98811_lab_week03/DetailFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.navigation.Navigation
 
 
@@ -35,8 +34,10 @@ class ListFragment : Fragment(){
                     R.id.coffee_id_action, fragmentBundle)
             )
         }
+
     }
     companion object {
         const val COFFEE_ID = "COFFEE_ID"
     }
+
 }
